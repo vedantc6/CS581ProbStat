c8e28af0a323911cf486f00cd92bb4de558f1f6d
vedantc6 <vedantc6@gmail.com>
2018-11-04 15:39:56 -0500

    Cleaning code

--- Assignment2/Assignment2.R
+++ Assignment2/Assignment2.R
@@ -1,3 +1,6 @@
+############################################################################
+########################## UTILITY FUNCTIONS ###############################
+############################################################################
 secondMomentCalc <- function(input_data, x_bar){
   temp = 0
   for (i in input_data){
@@ -14,23 +17,21 @@ firstMomentCalc <- function(input_data){
   return (temp/length(input_data))
 }
 
+############################################################################
+######### METHOD OF MOMENTS FUNCTIONS FOR DIFFERENT DISTRIBUTIONS ##########
+############################################################################
 mom_point <- function(input_data){
-  a = mean(input_data)
-  print(paste("First theoretical moment (Population mean) about origin:", a))
+  # a = mean(input_data)
+  # print(paste("First theoretical moment (Population mean) about origin:", a))
   # Estimating the parameters 
   a_hat = firstMomentCalc(input_data)
   print(paste("Estimated parameter 1 through MOM:", a_hat))
 }
 
-mom_uniform <- function(input_data){
-  mu_hat = mean(input_data)
-  print(mu_hat)
-  var_hat = secondMomentCalc(input_data, mu_hat)
-  print(var_hat)
-  a_hat = mu_hat - sqrt(3)*sqrt(var_hat)
-  b_hat = mu_hat + sqrt(3)*sqrt(var_hat)
-  print(paste("Estimated parameter 1 through MOM:", a_hat))
-  print(paste("Estimated parameter 2 through MOM:", b_hat))
+mom_bernoulli <- function(input_data){
+  # Estimating the parameters 
+  p_hat = firstMomentCalc(input_data)
+  print(paste("Estimated parameter 1 through MOM:", p_hat))
 }
 
 mom_binomial <- function(input_data){
@@ -42,12 +43,9 @@ mom_binomial <- function(input_data){
   print(paste("Estimated parameter 2 through MOM:", k_hat))
 }
 
-mom_bernoulli <- function(input_data){
-  p = mean(input_data)
-  print(paste("First theoretical moment (Population mean) about origin:", p))
-  # Estimating the parameters 
+mom_geometric <- function(input_data){ 
   p_hat = firstMomentCalc(input_data)
-  print(paste("Estimated parameter 1 through MOM:", p_hat))
+  print(paste("Estimated parameter 1 through MOM:", 1/p_hat))
 }
 
 mom_poisson <- function(input_data){ 
@@ -55,18 +53,16 @@ mom_poisson <- function(input_data){
   print(paste("Estimated parameter 1 through MOM:", lambda_hat))
 }
 
-mom_geometric <- function(input_data){ 
-  p_hat = firstMomentCalc(input_data)
-  print(paste("Estimated parameter 1 through MOM:", 1/p_hat))
+mom_uniform <- function(input_data){
+  mu_hat = firstMomentCalc(input_data)
+  var_hat = secondMomentCalc(input_data, mu_hat)
+  a_hat = mu_hat - sqrt(3)*sqrt(var_hat)
+  b_hat = mu_hat + sqrt(3)*sqrt(var_hat)
+  print(paste("Estimated parameter 1 through MOM:", a_hat))
+  print(paste("Estimated parameter 2 through MOM:", b_hat))
 }
 
 mom_normal <- function(input_data){
-  mu = mean(input_data)
-  variance = var(input_data)
-  th_2 = mu^2 + variance
-  print(paste("First theoretical moment (Population mean) about origin:", mu))
-  print(paste("Second theoretical moment about origin:", th_2))
-  print(paste("Population variance:", variance))
   # Estimating the parameters
   mu_hat = firstMomentCalc(input_data)
   var_hat = secondMomentCalc(input_data, mu_hat)
@@ -110,56 +106,58 @@ mom_chisq <- function(input_data){
   print(paste("Estimated parameter 1 through MOM:", 1/p_hat))
 }
 
-
+############################################################################
+########### MAIN CALL FUNCTION FOR METHOD OF MOMENTS FUNCTIONS #############
+############################################################################
 mom_wrapper <- function(input_data, distribution){
   if (distribution == "point"){
-    print("Point distribution has 1 parametes, hence 1st moment will give an estimator for a")
+    print("Point distribution has 1 parameter, hence 1st moment will give an estimator for a")
     estimator <- mom_point(input_data)
   }
   else if (distribution == "bernoulli"){
-    print("Bernoulli distribution only has 1 parameter, hence 1st moment will give an estimator for p")
+    print("Bernoulli distribution has 1 parameter, hence 1st moment will give an estimator for p")
     estimator <- mom_bernoulli(input_data)
   }
   else if (distribution == "binomial"){
-    print("Binomial distribution has 2 parameters, alpha and theta, hence we need to find first two moments to give an estimator for its parameters")
+    print("Binomial distribution has 2 parameters - p and k - hence first two moments will give its parameter estimates")
     estimator <- mom_binomial(input_data)
   }
   else if (distribution == "geometric"){
-    print("Geometric distribution has 2 parameters, alpha and theta, hence we need to find first two moments to give an estimator for its parameters")
+    print("Geometric distribution has 1 parameter, hence 1st moment will give an estimator for p")
     estimator <- mom_geometric(input_data)
   }
   else if (distribution == "poisson"){
-    print("Poisson distribution has 2 parameters, alpha and theta, hence we need to find first two moments to give an estimator for its parameters")
+    print("Poisson distribution has 1 parameter, hence 1st moment will give an estimator for lambda")
     estimator <- mom_poisson(input_data)
   }
   else if (distribution == "uniform"){
-    print("Uniform distribution has 2 parameters, alpha and theta, hence we need to find first two moments to give an estimator for its parameters")
+    print("Uniform distribution has 2 parameters - a and b - hence first two moments will give its parameter estimates")
     estimator <- mom_uniform(input_data)
   }
   else if (distribution == "normal"){
-    print("Normal distribution has 2 parameters, mu and sigma, hence we need to find first two moments to give an estimator for its parameters")
+    print("Normal distribution has 2 parameters - mu and sigma - hence first two moments will give its parameter estimates")
     estimator <- mom_normal(input_data)
   }
   else if (distribution == "exponential"){
-    print("Gamma distribution has 2 parameters, alpha and theta, hence we need to find first two moments to give an estimator for its parameters")
+    print("Exponential distribution has 1 parameter, hence 1st moment will give an estimator for theta")
     estimator <- mom_exponential(input_data)
   }
   else if (distribution == "gamma"){
-    print("Gamma distribution has 2 parameters, alpha and theta, hence we need to find first two moments to give an estimator for its parameters")
+    print("Gamma distribution has 2 parameters - alpha and theta - hence first two moments will give its parameter estimates")
     estimator <- mom_gamma(input_data)
   }
   else if (distribution == "beta"){
-    print("Gamma distribution has 2 parameters, alpha and theta, hence we need to find first two moments to give an estimator for its parameters")
+    print("Beta distribution has 2 parameters - alpha and beta - hence first two moments will give its parameter estimates")
     estimator <- mom_beta(input_data)
   }
   else if (distribution == "t"){
-    print("Gamma distribution has 2 parameters, alpha and theta, hence we need to find first two moments to give an estimator for its parameters")
+    print("T distribution has 1 parameter, hence the 1st moment will give an estimator for v (degree of freedoms)")
     estimator <- mom_t(input_data)
   }
-  # else if (distribution == "chi square"){
-  #   print("Gamma distribution has 2 parameters, alpha and theta, hence we need to find first two moments to give an estimator for its parameters")
-  #   estimator <- mom_gamma(input_data)
-  # }
+  else if (distribution == "chi square"){
+    print("Chi-Square distribution has 1 parameter, hence the 1st moment will give an estimator for p")
+    estimator <- mom_chisq(input_data)
+  }
   # else if (distribution == "multinomial"){
   #   print("Gamma distribution has 2 parameters, alpha and theta, hence we need to find first two moments to give an estimator for its parameters")
   #   estimator <- mom_gamma(input_data)
